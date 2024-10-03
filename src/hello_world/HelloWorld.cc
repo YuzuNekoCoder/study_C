facdb66bd79c79aa114fa987268daf4b3b2ddd2f
YuzuNeko <kojirodev0@gmail.com>
2024-10-03 17:17:23 +0900

    前方宣言を追加

--- src/hello_world/HelloWorld.cc
+++ src/hello_world/HelloWorld.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 
-// ソースコードは上から順に解釈されるため、呼び出しもとよりも上に記述する必要あり
+// ソースコードは上から順に解釈されるため、呼び出しもとよりも上に記述する必要ありだが、前方宣言で回避可能
+
+void SayHello();
 
 void HelloWorld(int n)
 {
@@ -13,12 +15,12 @@ void HelloWorld(int n)
 int main()
 {
     HelloWorld(10);
-    // SayHello(); これはコンパイルエラーになる
+    SayHello(); // 前方宣言しているためエラーなし。前方宣言なければコンパイルエラー
 
     return 0;
 }
 
-// void SayHello()
-// {
-//     std::cout << "Hello" << std::endl;
-// }
+void SayHello()
+{
+    std::cout << "Hello" << std::endl;
+}
