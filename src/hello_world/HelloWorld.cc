43e09ff5113fc28107856bdefca24ab97d84e30d
YuzuNeko <kojirodev0@gmail.com>
2024-10-03 17:14:02 +0900

    HelloWorld関数の追加

--- src/hello_world/HelloWorld.cc
+++ src/hello_world/HelloWorld.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 
+// ソースコードは上から順に解釈されるため、呼び出しもとよりも上に記述する必要あり
+
+void HelloWorld(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << "[" << i << "]" << "Hello World!" << std::endl;
+    }
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    HelloWorld(10);
+    // SayHello(); これはコンパイルエラーになる
 
     return 0;
 }
+
+// void SayHello()
+// {
+//     std::cout << "Hello" << std::endl;
+// }
