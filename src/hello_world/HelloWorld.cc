c7f6d180428402b12376c63a61ad902de637379e
YuzuNeko <kojirodev0@gmail.com>
2024-10-03 18:07:48 +0900

    関数ポインタの追加

--- src/hello_world/HelloWorld.cc
+++ src/hello_world/HelloWorld.cc
@@ -28,6 +28,11 @@ void Print(int x, double y)
     std::cout << "int: " << x << " & " << "double: " << y << std::endl;
 }
 
+int Add(int x, int y)
+{
+    return x + y;
+}
+
 int main()
 {
     HelloWorld(10);
@@ -36,6 +41,11 @@ int main()
     Print(2);
     Print(2.1);
     Print(1, 2.1);
+
+    // int (*fp)(int, int) = Add;        // 関数 Add のアドレスを保持する関数ポインタ fp
+    auto fp = Add;                    // 簡略化
+    int result = fp(3, 5);            // 関数ポインタ fp を介して関数 Add が実行される
+    std::cout << result << std::endl; // 8
 
     return 0;
 }
