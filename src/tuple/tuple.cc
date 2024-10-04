9c07e55a17ad1282cb446742731cb9315089d757
YuzuNeko <kojirodev0@gmail.com>
2024-10-04 14:43:07 +0900

    pairの追記

--- src/tuple/tuple.cc
+++ src/tuple/tuple.cc
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <iostream>
+#include <utility>
 
 int main()
 {
@@ -22,6 +23,36 @@ int main()
     std::tie(name, age, country) = person; // name: "Bob", age: 20, country: "USA"
 
     std::cout << "name =" << std::get<0>(person) << " " << "age =" << std::get<1>(person) << " " << "country =" << std::get<2>(person) << std::endl;
+
+    // ペア utilityのinclude必要
+    // タプル同様まとめて取り扱うことが可能
+
+    std::pair<std::string, int> personalInfo{"Bob", 20};
+
+    // 参照方法は以下のどちらか
+    auto get0 = std::get<0>(personalInfo); // "Bob"
+    auto get1 = std::get<1>(personalInfo); // 20
+
+    auto personalFirst = personalInfo.first;   // "Bob"
+    auto personalSecond = personalInfo.second; // 20
+
+    std::cout << "name =" << get0 << " " << "age =" << get1 << std::endl;
+    std::cout << "name =" << personalFirst << " " << "age =" << personalSecond << std::endl;
+
+    // 初期化のバリエーション
+    // std::pair<std::string, int> person{"Bob", 20};
+    // std::pair<std::string, int> person("Bob", 20);
+    // std::pair<std::string, int> person = std::make_pair("Bob", 20);
+
+    // まとめて代入
+    // std::tie() を使用
+
+    // std::pair<std::string, int> person{"Bob", 20};
+
+    // std::string name;
+    // int age;
+
+    // std::tie(name, age) = person; // name: "Bob", age: 20
 
     return 0;
 }
